c9201d5853fef1554568c6a89ab825bdbc837cad
kxob <58348680+kxob@users.noreply.github.com>
2026-07-22 16:47:35 +0800

    简化代码
    ```Java
    import de.robv.android.xposed.*;
    import static de.robv.android.xposed.XposedBridge.*;
    import static de.robv.android.xposed.XposedHelpers.*;
    import org.luckypray.dexkit.query.matchers.*;
    ```

--- app/src/main/java/XP/Fuck/MI/Fucking.java
+++ app/src/main/java/XP/Fuck/MI/Fucking.java
@@ -6,33 +6,32 @@ import android.view.View;
 
 import org.luckypray.dexkit.DexKitBridge;
 import org.luckypray.dexkit.query.FindMethod;
-import org.luckypray.dexkit.query.matchers.MethodMatcher;
+import org.luckypray.dexkit.query.enums.StringMatchType;
+import org.luckypray.dexkit.query.matchers.*;
 
 import java.io.File;
 import java.lang.reflect.Modifier;
 import java.util.List;
 
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import de.robv.android.xposed.*;
+import static de.robv.android.xposed.XposedBridge.*;
+import static de.robv.android.xposed.XposedHelpers.*;
 
-public class Fucking implements IXposedHookLoadPackage {
+public class Fucker implements IXposedHookLoadPackage {
     static {
         System.loadLibrary("dexkit");
     }
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) {
-        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+        findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 ClassLoader classLoader = ((Context) param.args[0]).getClassLoader();
                 DexKitBridge bridge = DexKitBridge.create(classLoader, true);
                 //＠下载管理：安装就安装，不要瞎启动！
                 if (lpparam.packageName.equals("com.android.providers.downloads.ui")) {
-                    XposedBridge.hookMethod(bridge.findMethod(FindMethod.create().matcher(MethodMatcher.create().modifiers(Modifier.PUBLIC | Modifier.STATIC).paramTypes("android.content.Context", "java.lang.String").returnType("int").addInvoke(MethodMatcher.create().name("getPackageInfo")))).single().getMethodInstance(classLoader), new XC_MethodReplacement() {
+                    hookMethod(bridge.findMethod(FindMethod.create().matcher(MethodMatcher.create().modifiers(Modifier.PUBLIC | Modifier.STATIC).paramTypes(Context.class, String.class).returnType(int.class).addInvoke(MethodMatcher.create().name("getPackageInfo")))).single().getMethodInstance(classLoader), new XC_MethodReplacement() {
                         @Override
                         protected Object replaceHookedMethod(MethodHookParam param) {
                             return -1;
@@ -41,75 +40,89 @@ public class Fucking implements IXposedHookLoadPackage {
                 }
                 //＠自动连招：解除数值大小限制
                 if (lpparam.packageName.equals("com.xiaomi.macro")) {
-                    XposedHelpers.findAndHookMethod("com.xiaomi.macro.main.view.MainMacro", classLoader, "handlePlayParameterConfirm", new XC_MethodHook() {
+                    findAndHookMethod("com.xiaomi.macro.main.view.MainMacro", classLoader, "handlePlayParameterConfirm", new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) {
                             param.setResult(null);
-                            String inputValue = XposedHelpers.callMethod(XposedHelpers.getObjectField(param.thisObject, "mEtPlayParameter"), "getText").toString();
-                            Object macro = ((List<?>) XposedHelpers.getObjectField(param.thisObject, "mMacroList")).get(XposedHelpers.getIntField(param.thisObject, "mSelectedPosition"));
-                            int playParameterFlag = XposedHelpers.getIntField(param.thisObject, "mPlayParameterFlag");
+                            String inputValue = callMethod(getObjectField(param.thisObject, "mEtPlayParameter"), "getText").toString();
+                            Object macro = ((List<?>) getObjectField(param.thisObject, "mMacroList")).get(getIntField(param.thisObject, "mSelectedPosition"));
+                            int playParameterFlag = getIntField(param.thisObject, "mPlayParameterFlag");
                             switch (playParameterFlag) {
                                 case 1: // 播放速度
-                                    XposedHelpers.callMethod(macro, "setPlaySpeed", Double.parseDouble(inputValue));
+                                    callMethod(macro, "setPlaySpeed", Double.parseDouble(inputValue));
                                     break;
                                 case 2: // 播放次数
-                                    XposedHelpers.callMethod(macro, "setPlayTimes", Integer.parseInt(inputValue));
+                                    callMethod(macro, "setPlayTimes", Integer.parseInt(inputValue));
                                     break;
                                 case 3: // 播放延迟
-                                    XposedHelpers.callMethod(macro, "setPlayDelay", Integer.parseInt(inputValue));
+                                    callMethod(macro, "setPlayDelay", Integer.parseInt(inputValue));
                                     break;
                             }
-                            XposedHelpers.callMethod(XposedHelpers.getObjectField(param.thisObject, "mMainMacroPresenter"), "updateMacro", macro);
-                            Object macroWindowManager = XposedHelpers.getObjectField(param.thisObject, "mMacroWindowManager");
-                            XposedHelpers.callMethod(macroWindowManager, "removeView", XposedHelpers.getObjectField(macroWindowManager, "mPlayParameterView"));
+                            callMethod(getObjectField(param.thisObject, "mMainMacroPresenter"), "updateMacro", macro);
+                            Object macroWindowManager = getObjectField(param.thisObject, "mMacroWindowManager");
+                            callMethod(macroWindowManager, "removeView", getObjectField(macroWindowManager, "mPlayParameterView"));
                         }
                     });
-                    XposedHelpers.findAndHookMethod("com.xiaomi.macro.widget.ClearEditText", classLoader, "onTextChanged", CharSequence.class, int.class, int.class, int.class, new XC_MethodReplacement() {
+                    findAndHookMethod("com.xiaomi.macro.widget.ClearEditText", classLoader, "onTextChanged", CharSequence.class, int.class, int.class, int.class, new XC_MethodReplacement() {
                         @Override
                         protected Object replaceHookedMethod(MethodHookParam param) {
                             return null;
                         }
                     });
                     //移除按键一秒CD
-                    XposedHelpers.findAndHookMethod("com.xiaomi.macro.using.view.UsingMacro", classLoader, "lambda$initListener$0$UsingMacro", XposedHelpers.findClass("com.xiaomi.macro.widget.DrawMacro", classLoader), View.class, new XC_MethodHook() {
+                    findAndHookMethod("com.xiaomi.macro.using.view.UsingMacro", classLoader, "lambda$initListener$0$UsingMacro", findClass("com.xiaomi.macro.widget.DrawMacro", classLoader), View.class, new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) {
-                            XposedHelpers.setLongField(param.thisObject, "mStopTime", 0L);
+                            setLongField(param.thisObject, "mStopTime", 0L);
                         }
                     });
                 }
-                //＠手机／平板管家：屏蔽导致 Hunter 出现 SafetyDetectClient 异常项的内鬼服务
+                //＠手机／平板管家：屏蔽导致 Hunter 出现 SafetyDetectClient 异常项的内鬼服务；使自动连招支持所有应用
                 if (lpparam.packageName.equals("com.miui.securitycenter")) {
-                    XposedBridge.hookMethod(bridge.findMethod(FindMethod.create().matcher(MethodMatcher.create().modifiers(Modifier.PUBLIC).name("<init>").paramTypes("int", null, "com.xiaomi.security.xsof.IMiSafetyDetectCallback"))).single().getConstructorInstance(classLoader), new XC_MethodHook() {
+                    hookMethod(bridge.findMethod(FindMethod.create().matcher(MethodMatcher.create().modifiers(Modifier.PUBLIC).name("<init>").paramTypes("int", null, "com.xiaomi.security.xsof.IMiSafetyDetectCallback"))).single().getConstructorInstance(classLoader), new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) {
                             param.args[0] = 11;
                         }
                     });
-                }
-                //＠剪贴板与常用语：拒绝区别对待输入法
-                if (lpparam.packageName.equals("com.miui.phrase")) {
-                    XposedHelpers.findAndHookMethod("com.miui.inputmethod.InputMethodBottomManager", classLoader, "getSupportIme", new XC_MethodReplacement() {
+                    hookMethod(bridge.findMethod(FindMethod.create().matcher(MethodMatcher.create().declaredClass(ClassMatcher.create().addUsingString("setCanPlay", StringMatchType.StartsWith)).addInvoke(MethodMatcher.create().declaredClass(ClassMatcher.create().addMethod(MethodMatcher.create().paramTypes(View.class).returnType("android.view.SurfaceControl"))).returnType(boolean.class).usingNumbers(90, 270)))).single().getMethodInstance(classLoader), new XC_MethodHook() {
                         @Override
-                        protected Object replaceHookedMethod(MethodHookParam param) {
-                            Object sBottomViewHelper = XposedHelpers.getStaticObjectField(param.thisObject.getClass(), "sBottomViewHelper");
-                            Object mImm = XposedHelpers.getObjectField(sBottomViewHelper, "mImm");
-                            return XposedHelpers.callMethod(mImm, "getEnabledInputMethodList");
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
                         }
                     });
                 }
-                //＠系统桌面：谁教你一碰到图标就预启动应用的？
-                if (lpparam.packageName.equals("com.miui.home")) {
-                    XposedHelpers.findAndHookMethod("com.miui.home.launcher.util.PreLaunchAppUtil", classLoader, "preLaunchProcess", XposedHelpers.findClass("com.miui.home.launcher.ShortcutInfo", classLoader), new XC_MethodHook() {
+                //＠剪贴板与常用语：拒绝区别对待输入法
+                if (lpparam.packageName.equals("com.miui.phrase")) {
+                    Class<?> InputMethodBottomManager = findClass("com.miui.inputmethod.InputMethodBottomManager", classLoader);
+                    findAndHookMethod(InputMethodBottomManager, "getSupportIme", new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) {
-                            XposedHelpers.setStaticBooleanField(param.thisObject.getClass(), "isEnableTouchDown", false);
+                            Object sBottomViewHelper = getStaticObjectField(InputMethodBottomManager, "sBottomViewHelper");
+                            Object mImm = getObjectField(sBottomViewHelper, "mImm");
+                            param.setResult(callMethod(mImm, "getEnabledInputMethodList"));
+
+                        }
+                    });
+                    /*findAndHookMethod(Iterator.class, "remove", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) {
+                            for (StackTraceElement element : Thread.currentThread().getStackTrace()) if (element.getClassName().equals("com.miui.inputmethod.InputMethodBottomManager") && element.getMethodName().equals("getSupportIme")) param.setResult(null);
+                        }
+                    });*/
+                }
+                //＠系统桌面：谁教你一碰到图标就预启动应用的？
+                if (lpparam.packageName.equals("com.miui.home")) {
+                    findAndHookMethod("com.miui.home.launcher.util.PreLaunchAppUtil", classLoader, "preLaunchProcess", findClass("com.miui.home.launcher.ShortcutInfo", classLoader), new XC_MethodReplacement() {
+                        @Override
+                        protected Object replaceHookedMethod(MethodHookParam param) {
+                            return false;
                         }
                     });
                 }
                 //＠相册：禁止存储缩略图（/sdcard/Android/data/com.miui.gallery/files/gallery_disk_cache）占用大量空间
                 if (lpparam.packageName.equals("com.miui.gallery")) {
-                    XposedHelpers.findAndHookMethod("com.miui.gallery.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory$1", classLoader, "getCacheDirectory", new XC_MethodReplacement() {
+                    findAndHookMethod("com.miui.gallery.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory$1", classLoader, "getCacheDirectory", new XC_MethodReplacement() {
                         @Override
                         protected Object replaceHookedMethod(MethodHookParam param) {
                             return null;
@@ -118,13 +131,13 @@ public class Fucking implements IXposedHookLoadPackage {
                 }
                 //＠媒体选择工具：禁用.globalTrash
                 if (lpparam.packageName.equals("com.android.providers.media.module")) {
-                    XposedHelpers.findAndHookMethod("com.android.providers.media.util.GalleryMediaStore", classLoader, "initGlobalTrash", Context.class, new XC_MethodReplacement() {
+                    findAndHookMethod("com.android.providers.media.util.GalleryMediaStore", classLoader, "initGlobalTrash", Context.class, new XC_MethodReplacement() {
                         @Override
                         protected Object replaceHookedMethod(MethodHookParam param) {
                             return null;
                         }
                     });
-                    XposedHelpers.findAndHookMethod("com.android.providers.media.MediaReceiverInjector", classLoader, "initDragImgsDir", File.class, new XC_MethodReplacement() {
+                    findAndHookMethod("com.android.providers.media.MediaReceiverInjector", classLoader, "initDragImgsDir", File.class, new XC_MethodReplacement() {
                         @Override
                         protected Object replaceHookedMethod(MethodHookParam param) {
                             return null;
